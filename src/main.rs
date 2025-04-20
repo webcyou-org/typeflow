eedc1cceac7e1c1b382cbf7f0816b1ac09965d7c
panicdragon <d.takayama.web@gmail.com>
2025-04-20 17:00:19 +0900

    refactor: 💡 multi message flow

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,33 @@ use typeflow::CharFlow;
 
 #[derive(Parser, Debug)]
 struct Args {
-    message: String,
+    #[arg(required = true)]
+    messages: Vec<String>,
     #[arg(short, long, default_value_t = 50)]
     delay: u64,
 }
 
 fn main() {
     let args = Args::parse();
-    let flow = CharFlow::new(&args.message, args.delay);
 
-    for ch in flow {
-        print!("{}", ch);
-        std::io::stdout().flush().unwrap();
+    if args.messages.len() == 1 {
+        let flow = CharFlow::new(&args.messages.first().unwrap(), args.delay);
+        for ch in flow {
+            print!("{}", ch);
+            std::io::stdout().flush().unwrap();
+        }
+        println!();
+    } else {
+        for message in args.messages {
+            let flow = CharFlow::new(&message, args.delay);
+
+            for ch in flow {
+                print!("{}", ch);
+                std::io::stdout().flush().unwrap();
+            }
+
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+        }
     }
-    println!();
 }
-
